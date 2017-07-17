ca068dd20c9f580bfc8fe804fdd0490347e31123
oscar <1208442830@qq.com>
2017-07-17 20:46:33 +0800

    commit

---
+++ src/application/detail/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="">
+        {{ msg }}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'detail',
+  data() {
+    return {
+      msg: '商品详情',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,47 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
+import Main from '@/application/main'
 import Home from '@/application/index'
 import Catergry from '@/application/catergry'
 import Find from '@/application/find'
 import Cart from '@/application/cart'
 import User from '@/application/user'
+import Detail from '@/application/detail'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  linkActiveClass:'active',
-  routes: [
-    {
-      name: 'Main',
-      path: '/',
-      component: '',
-      children: [{
-        path: '',
-        name: 'Home',
-        component: Home
-      }, {
-        path: '/catergry',
-        name: 'Catergry',
-        component: Catergry
-      }, {
-        path: '/find',
-        name: 'Find',
-        component: Find
-      }, {
-        path: '/user/:id',
-        name: 'User',
-        component: User
-      }]
+  linkActiveClass: 'active',
+  routes: [{
+    name: 'Main',
+    path: '/',
+    component: Main,
+    children: [{
+      path: '',
+      name: 'Home',
+      component: Home
     }, {
-      path: '/detail?:id',
-      name: 'Detail',
-      component: 'Detail'
-    },
-    {
-      path: '/cart',
-      name: 'Cart',
-      component: Cart
-    }
-  ]
+      path: '/catergry',
+      name: 'Catergry',
+      component: Catergry
+    }, {
+      path: '/find',
+      name: 'Find',
+      component: Find
+    }, {
+      path: '/user/:id',
+      name: 'User',
+      component: User
+    }]
+  }, {
+    path: '/detail?:id',
+    name: 'Detail',
+    component: Detail
+  }, {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
+  }]
 })
